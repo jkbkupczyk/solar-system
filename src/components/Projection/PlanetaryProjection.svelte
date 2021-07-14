494eac712180384c20ad44cb946fdd3de5dd1114
Jakub Kupczyk <jkbkupczyk@gmail.com>
2021-07-14 23:27:02 +0200

    Added planetary projection component

---
+++ src/components/Projection/PlanetaryProjection.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { onMount } from "svelte";
+    import { Projection } from "./app/Projection";
+
+    export let props;
+    let canvas
+
+    onMount(() => {
+        const projection = new Projection(canvas, props.data);
+    });
+</script>
+
+<div class="absolute w-full h-full">
+    <canvas id="canvas" class="block inset-0 w-full h-full overflow-hidden" bind:this={canvas} />
+</div>
+
+<style>
+    #canvas {
+        z-index: -1;
+    }
+</style>
