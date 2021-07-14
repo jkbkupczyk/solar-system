781ba646123bc5a993545f7e31a95cd968b58cc3
Jakub Kupczyk <jkbkupczyk@gmail.com>
2021-07-14 23:31:12 +0200

    Added data files for astronomical data

---
+++ src/data/planetarySystems.js
@@ -0,0 +1,27 @@
+// import { solarData } from "./solar.json";
+// import { proximaData } from "./proxima.json";
+// import { trappistData } from "./trappist.json";
+
+export const planetarySystems = [
+    {
+        id: 1,
+        name: "Solar System",
+        desc: "Lorem dolor sit",
+        // data: solarData,
+        img: "/images/sun.webp"
+    },
+    {
+        id: 2,
+        name: "Proxima System",
+        desc: "Lorem dolor sit",
+        // data: proximaData,
+        img: "/images/proxima.webp"
+    },
+    {
+        id: 3,
+        name: "Trappist System",
+        desc: "Lorem dolor sit",
+        // data: trappistData,
+        img: "/images/trappist.webp"
+    },
+];
