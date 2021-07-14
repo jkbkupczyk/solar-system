a0568b5968b4d41198e3b9d963acf03369238986
Jakub Kupczyk <jkbkupczyk@gmail.com>
2021-07-14 23:32:09 +0200

    Added pages components for SPA

---
+++ src/pages/Home.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Header from "../components/Header/Header.svelte";
+    import Main from "../components/Main/Main.svelte";
+    import Footer from "../components/Footer/Footer.svelte";
+</script>
+
+<Header />
+<Main />
+<Footer />
