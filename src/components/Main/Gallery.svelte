ee2c3bc461d102bff559ad5fbc52ceaed6506aa8
Jakub Kupczyk <jkbkupczyk@gmail.com>
2021-07-14 23:34:52 +0200

    Added Gallery component

---
+++ src/components/Main/Gallery.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Divider from "../UI/Divider.svelte";
+    import Slider from "./Slider/Slider.svelte";
+</script>
+
+<article>
+    <Divider text="gallery" />
+    <div class="p-3 md:p-8">
+        <Slider />
+    </div>
+</article>
